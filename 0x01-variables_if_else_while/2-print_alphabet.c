e9514c02042df06c0b9258fff22216847c1a14a5
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-04-09 03:34:00 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,14 @@
 int main(void)
 
 {
-  char alphabet;
+char alphabet;
 
-  for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-    {
-      putchar(alphabet);
-    }
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
 
-  putchar('\n');
+putchar('\n');
 
-  return (0);
+return (0);
 }
