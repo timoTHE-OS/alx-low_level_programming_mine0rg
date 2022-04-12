5d1bd49b19ead136d63c69f5a69613255f415e78
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-04-12 19:27:16 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,19 @@
 
 void print_alphabet_x10(void)
 {
-  char c;
-  int i = 0;
+char c;
+int i = 0;
 
-  while (i < 10)
-    {
-      c = 'a';
-      while (c <= 'z')
-	{
-	  _putchar(c);
-	  c++;
-	}
-      _putchar('\n');
-      i++;
-    }
+while (i < 10)
+{
+c = 'a';
+while (c <= 'z')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
+i++;
+}
 
 }
