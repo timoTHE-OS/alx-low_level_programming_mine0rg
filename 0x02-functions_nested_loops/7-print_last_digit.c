2d7b104ec0c06341ae42d62879d3903400219122
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-04-12 19:30:53 +0100

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,14 @@
 
 int print_last_digit(int n)
 {
-  int last;
+int last;
 
-  last = n % 10;
+last = n % 10;
 
-  if (last < 0)
-    last = last * -1;
+if (last < 0)
+last = last * -1;
 
-  _putchar(last + '0');
+_putchar(last + '0');
 
-  return (last);
+return (last);
 }
