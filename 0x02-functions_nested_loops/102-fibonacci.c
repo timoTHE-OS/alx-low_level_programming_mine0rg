d2f86c2177e12863006e2920dd5698c945801a4a
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-04-12 20:01:55 +0100

    commit updated for 102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,22 +7,22 @@
  */
 int main(void)
 {
-  int i = 0;
-  long int a = 0, b = 1, next;
+int i = 0;
+long int a = 0, b = 1, next;
 
-  while (i < 50)
-    {
-      next = a + b;
-      a = b;
-      b = next;
-      printf("%lu", next);
+while (i < 50)
+{
+next = a + b;
+a = b;
+b = next;
+printf("%lu", next);
 
-      if (i < 49)
-	{
-	  printf(", ");
-	}
-      i++;
-    }
-  putchar('\n');
-  return (0);
+if (i < 49)
+{
+printf(", ");
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
