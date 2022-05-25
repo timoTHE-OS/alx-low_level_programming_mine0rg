7445f1821c621963c863aa207cf04760bd63367f
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-05-25 01:49:37 +0100

    Update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,8 +17,8 @@ if (new == NULL)
 return (NULL);
 
 new->str = strdup(str);
-for (nchar = 0; str[nchar]; nchar++)
-  ;
+for (nchar = 0; str[nchar];)
+nchar++;
 
 new->len = nchar;
 new->next = *head;
