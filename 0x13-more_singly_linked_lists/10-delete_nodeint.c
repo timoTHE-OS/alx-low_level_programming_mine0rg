a5c2d01ed92079cbc56ff0bb7567a76458db82c0
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-05-27 22:08:54 +0100

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,4 +1,4 @@
-include "lists.h"
+#include "lists.h"
 
 /**
  * delete_nodeint_at_index - deletes the node at index
@@ -11,37 +11,37 @@ include "lists.h"
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-  unsigned int i;
-  listint_t *prev;
-  listint_t *next;
+unsigned int i;
+listint_t *prev;
+listint_t *next;
 
-  prev = *head;
+prev = *head;
 
-  if (index != 0)
-    {
-      for (i = 0; i < index - 1 && prev != NULL; i++)
-	{
-	  prev = prev->next;
-	}
-    }
+if (index != 0)
+{
+for (i = 0; i < index - 1 && prev != NULL; i++)
+{
+prev = prev->next;
+}
+}
 
-  if (prev == NULL || (prev->next == NULL && index != 0))
-    {
-      return (-1);
-    }
+if (prev == NULL || (prev->next == NULL && index != 0))
+{
+return (-1);
+}
 
-  next = prev->next;
+next = prev->next;
 
-  if (index != 0)
-    {
-      prev->next = next->next;
-      free(next);
-    }
-  else
-    {
-      free(prev);
-      *head = next;
-    }
+if (index != 0)
+{
+prev->next = next->next;
+free(next);
+}
+else
+{
+free(prev);
+*head = next;
+}
 
-  return (1);
+return (1);
 }
