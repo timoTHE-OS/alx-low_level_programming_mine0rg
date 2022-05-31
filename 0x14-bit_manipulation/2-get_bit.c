52de530e29a30874b95643324aec415a9ef26995
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-05-31 05:08:47 +0100

    Update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,11 +10,11 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-  if (index >= (sizeof(unsigned long int) * 8))
-    return (-1);
+if (index >= (sizeof(unsigned long int) * 8))
+return (-1);
 
-  if ((n & (1 << index)) == 0)
-    return (0);
+if ((n & (1 << index)) == 0)
+return (0);
 
-  return (1);
+return (1);
 }
