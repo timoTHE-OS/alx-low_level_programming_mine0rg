02d10fe391b6e1a6598de2e6d99901655eb3828f
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-05-31 05:10:13 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,13 +10,13 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-  unsigned long int xor = n ^ m, bits = 0;
+unsigned long int xor = n ^ m, bits = 0;
 
-  while (xor > 0)
-    {
-      bits += (xor & 1);
-      xor >>= 1;
-    }
+while (xor > 0)
+{
+bits += (xor & 1);
+xor >>= 1;
+}
 
-  return (bits);
+return (bits);
 }
