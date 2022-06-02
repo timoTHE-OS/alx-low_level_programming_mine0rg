6b0d3177901c575a70890bd3f45e104480f17fea
timoTHE-OS <101520988+timoTHE-OS@users.noreply.github.com>
2022-06-02 05:31:44 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,63 +9,27 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-  int fd;
-  ssize_t lenr, lenw;
-  char *buffer;
+int fd;
+ssize_t lenr, lenw;
+char *buffer;
 
-  if (!filename)
-    return (0);
+if (!filename)
+return (0);
 
-  fd = open(filename, O_RDONLY);
+fd = open(filename, O_RDONLY);
+if (fd == -1)
+return (0);
 
-  if (fd == -1)
-    return (0);
+buffer = malloc(sizeof(char) * (letters));
+if (!buffer)
+return (0);
 
-  buffer = malloc(sizeof(char) * (letters));
-  if (!buffer)
-    return (0);
+lenr = read(fd, buffer, letters);
+lenw = write(STDOUT_FILENO, buffer, lenr);
 
-  lenr = read(fd, buffer, letters);
-  lenw = write(STDOUT_FILENO, buffer, lenr);
+close(fd);
 
-  close(fd);
+free(buffer);
 
-  free(buffer);
-
-  return (lenw);
-#include "main.h"
-
-/**
- * read_textfile - reads a text file and prints the letters
- * @filename: filename.
- * @letters: numbers of letters printed.
- *
- * Return: numbers of letters printed. It fails, returns 0.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	ssize_t lenr, lenw;
-	char *buffer;
-
-	if (!filename)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
-		return (0);
-
-	buffer = malloc(sizeof(char) * (letters));
-	if (!buffer)
-		return (0);
-
-	lenr = read(fd, buffer, letters);
-	lenw = write(STDOUT_FILENO, buffer, lenr);
-
-	close(fd);
-
-	free(buffer);
-
-	return (lenw);
+return (lenw);
 }
